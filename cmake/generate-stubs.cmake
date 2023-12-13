68017db4d2eb0f307f207f3525f9a3f244b77cee
friendlyanon <friendlyanon@users.noreply.github.com>
2023-12-13 23:01:22 +0100

    Generate winmm.dll thunks in .asm
    This is done only for my own curiosity and to try something new, but
    this also managed to make the resulting .DLL smaller in size, so this is
    a win-win!

--- cmake/generate-stubs.cmake
+++ cmake/generate-stubs.cmake
@@ -4,17 +4,29 @@ file(READ "${input}" functions)
 string(STRIP "${functions}" functions)
 string(REPLACE "\n" \; functions "${functions}")
 
+set(externs "")
 set(variables "")
 set(thunks "")
 set(calls "")
 set(def "EXPORTS\n")
 
 foreach(fn IN LISTS functions)
+  string(APPEND externs "EXTERN ${fn}_ptr:DWORD\n")
   string(APPEND variables "FARPROC ${fn}_ptr = NULL;\n")
-  string(APPEND thunks "__declspec(naked) void _${fn}(void) { _asm { jmp[${fn}_ptr] } }\n")
+  string(APPEND thunks "\n_${fn} PROC\n  jmp dword ptr [${fn}_ptr]\n_${fn} ENDP\n")
   string(APPEND calls "\n      || get_fn(&${fn}_ptr, \"${fn}\")")
   string(APPEND def "  ${fn} = _${fn}\n")
 endforeach()
+
+file(WRITE "${output}/stubs-generated.asm" "\
+.386
+.MODEL FLAT, C
+
+${externs}
+.CODE
+${thunks}
+END
+")
 
 file(WRITE "${output}/stubs-generated.c" "\
 #include <Windows.h>
@@ -23,7 +35,6 @@ file(WRITE "${output}/stubs-generated.c" "\
 #include \"stubs.h\"
 
 ${variables}
-${thunks}
 static int get_fn(FARPROC* out, LPCSTR name)
 {
   *out = GetProcAddress((HMODULE)winmm_handle, name);
